bcdd3b70b39cde9d9b8e5e54992ccf48eda81741
eastonyarbrough <easton.yarbrough@bryanuniversity.edu>
2022-09-06 00:11:58 -0500

    Complete 'Recalling Python Functions' assignment

---
+++ week1/recallPythonFunc.py
@@ -0,0 +1,30 @@
+class DataTypes:
+    def __init__(self):
+        self.types = []
+    
+    def add(self, type):
+        self.types.append(type)
+
+    def addNum(self, num):
+        self.add(int(num))
+
+    def addStr(self, string):
+        self.add(str(string))
+
+    def addDict(self, list):
+        self.add({list[i]: list[i + 1] for i in range(0, len(list), 2)})
+
+    def addTuple(self, tupe):
+        self.add(tuple(tupe))
+
+    def getList(self):
+        return self.types
+
+myList = DataTypes()
+
+myList.addNum('2')
+myList.addStr(13)
+myList.addDict(['a', 1, 'b', 2, 'c', 3])
+myList.addTuple('Hello')
+
+print(myList.getList())
